35e48b8eabced7a851973e3acf698bf3ac4df793
Zheludkov Egor <zheludkov.egor@mail.ru>
2020-04-08 00:57:25 +0300

    fix logic new color slider

--- Egorozh.ColorPicker.WPF/Controls/ColorEditor/ColorSliderNew.cs
+++ Egorozh.ColorPicker.WPF/Controls/ColorEditor/ColorSliderNew.cs
@@ -1,10 +1,20 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 
 namespace Egorozh.ColorPicker
 {
     internal class ColorSliderNew : Slider
     {
+        #region Private Fields
+
+        private Thumb _thumb = null;
+        private bool _isDownOnSlider = false;
+
+        #endregion
+
         #region Dependency Properties
 
         public static readonly DependencyProperty BarStyleProperty = DependencyProperty.Register(
@@ -39,6 +49,66 @@ namespace Egorozh.ColorPicker
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorSliderNew),
                 new FrameworkPropertyMetadata(typeof(ColorSliderNew)));
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (_thumb != null)
+            {
+                _thumb.MouseEnter -= thumb_MouseEnter;
+                _thumb.LostMouseCapture -= thumb_LostMouseCapture;
+            }
+
+            if (GetTemplateChild("PART_Track") is Track track)
+            {
+                _thumb = track.Thumb;
+
+                if (_thumb != null)
+                {
+                    _thumb.MouseEnter += thumb_MouseEnter;
+                    _thumb.LostMouseCapture += thumb_LostMouseCapture;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonDown(e);
+            _isDownOnSlider = true;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void thumb_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && _isDownOnSlider)
+            {
+                // the left button is pressed on mouse enter 
+                // so the thumb must have been moved under the mouse 
+                // in response to a click on the track. 
+                // Generate a MouseLeftButtonDown event. 
+                MouseButtonEventArgs args = new MouseButtonEventArgs(
+                    e.MouseDevice, e.Timestamp, MouseButton.Left);
+                args.RoutedEvent = MouseLeftButtonDownEvent;
+                (sender as Thumb).RaiseEvent(args);
+            }
+        }
+
+        private void thumb_LostMouseCapture(object sender, EventArgs e)
+        {
+            _isDownOnSlider = false;
         }
 
         #endregion
