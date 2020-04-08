f7d6115775752940bf13c4da548064b6223d1a68
Zheludkov Egor <zheludkov.egor@mail.ru>
2020-04-08 12:21:58 +0300

    ported hue slider

--- Egorozh.ColorPicker.WPF/Controls/ColorEditor/ColorSliderNew.cs
+++ Egorozh.ColorPicker.WPF/Controls/ColorEditor/ColorSliderNew.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Egorozh.ColorPicker
 {
@@ -19,11 +22,26 @@ namespace Egorozh.ColorPicker
 
         public static readonly DependencyProperty BarStyleProperty = DependencyProperty.Register(
             nameof(BarStyle), typeof(ColorBarStyle), typeof(ColorSliderNew),
-            new PropertyMetadata(default(ColorBarStyle)));
+            new PropertyMetadata(default(ColorBarStyle), BarStyleChanged));
+
+        private static void BarStyleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ColorSliderNew slider)
+                slider.PaintBar();
+        }
 
         public static readonly DependencyProperty CustomColorsProperty = DependencyProperty.Register(
             nameof(CustomColors), typeof(ColorCollectionNew), typeof(ColorSliderNew),
-            new PropertyMetadata(default(ColorCollectionNew)));
+            new PropertyMetadata(default(ColorCollectionNew), CustomColorsChanged));
+
+        private static void CustomColorsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ColorSliderNew slider)
+                slider.PaintBar();
+        }
+
+        public static readonly DependencyProperty BarBrushProperty = DependencyProperty.Register(
+            nameof(BarBrush), typeof(Brush), typeof(ColorSliderNew), new PropertyMetadata(default(Brush)));
 
         #endregion
 
@@ -39,6 +57,12 @@ namespace Egorozh.ColorPicker
         {
             get => (ColorCollectionNew) GetValue(CustomColorsProperty);
             set => SetValue(CustomColorsProperty, value);
+        }
+
+        public Brush BarBrush
+        {
+            get => (Brush) GetValue(BarBrushProperty);
+            set => SetValue(BarBrushProperty, value);
         }
 
         #endregion
@@ -60,20 +84,14 @@ namespace Egorozh.ColorPicker
             base.OnApplyTemplate();
 
             if (_thumb != null)
-            {
-                _thumb.MouseEnter -= thumb_MouseEnter;
                 _thumb.LostMouseCapture -= thumb_LostMouseCapture;
-            }
 
             if (GetTemplateChild("PART_Track") is Track track)
             {
                 _thumb = track.Thumb;
 
                 if (_thumb != null)
-                {
-                    _thumb.MouseEnter += thumb_MouseEnter;
                     _thumb.LostMouseCapture += thumb_LostMouseCapture;
-                }
             }
         }
 
@@ -84,31 +102,129 @@ namespace Egorozh.ColorPicker
         protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
         {
             base.OnPreviewMouseLeftButtonDown(e);
+
             _isDownOnSlider = true;
+            Mouse.Capture(_thumb);
+            _thumb.PreviewMouseMove += _thumb_PreviewMouseMove;
+        }
+
+        private void _thumb_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isDownOnSlider && !_thumb.IsDragging && e.LeftButton == MouseButtonState.Pressed)
+            {
+                var args = new MouseButtonEventArgs(e.MouseDevice, e.Timestamp, MouseButton.Left)
+                {
+                    RoutedEvent = MouseLeftButtonDownEvent
+                };
+
+                Mouse.Capture(null);
+                _thumb.PreviewMouseMove -= _thumb_PreviewMouseMove;
+                _isDownOnSlider = false;
+
+                _thumb.RaiseEvent(args);
+            }
         }
 
         #endregion
 
         #region Private Methods
 
-        private void thumb_MouseEnter(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed && _isDownOnSlider)
-            {
-                // the left button is pressed on mouse enter 
-                // so the thumb must have been moved under the mouse 
-                // in response to a click on the track. 
-                // Generate a MouseLeftButtonDown event. 
-                MouseButtonEventArgs args = new MouseButtonEventArgs(
-                    e.MouseDevice, e.Timestamp, MouseButton.Left);
-                args.RoutedEvent = MouseLeftButtonDownEvent;
-                (sender as Thumb).RaiseEvent(args);
-            }
-        }
-
         private void thumb_LostMouseCapture(object sender, EventArgs e)
         {
             _isDownOnSlider = false;
+        }
+
+        private void PaintBar()
+        {
+            double angle = Orientation == Orientation.Horizontal ? 0 : 90;
+
+            var brush = new LinearGradientBrush();
+            
+            switch (BarStyle)
+            {
+                case ColorBarStyle.TwoColor:
+                    break;
+                case ColorBarStyle.ThreeColor:
+                    break;
+                case ColorBarStyle.Custom:
+
+                    var custom = CustomColors;
+                    var count = custom?.Count ?? 0;
+
+                    if (custom != null && count > 0)
+                    {
+                        var grs = custom.Select((color, i) =>
+                            new GradientStop(color, i == 0 ? 0 : i == count - 1 ? 1 : 1.0D / count * i));
+
+                        brush = new LinearGradientBrush(new GradientStopCollection(grs), angle);
+                    }
+                    else
+                    {
+                        //blend.Colors = new[]
+                        //{
+                        //                    this.Color1,
+                        //                    this.Color2
+                        //                };
+                        //blend.Positions = new[]
+                        //{
+                        //                    0F,
+                        //                    1F
+                        //                };
+                    }
+
+                    break;
+            }
+            
+            BarBrush = brush;
+
+            //float angle;
+
+            //angle = this.Orientation == Orientation.Horizontal ? 0 : 90;
+
+            //if (this.BarBounds.Height > 0 && this.BarBounds.Width > 0)
+            //{
+            //    ColorBlend blend;
+
+            //    // HACK: Inflating the brush rectangle by 1 seems to get rid of a odd issue where the last color is drawn on the first pixel
+
+            //    blend = new ColorBlend();
+            //    using (LinearGradientBrush brush = new LinearGradientBrush(Rectangle.Inflate(this.BarBounds, 1, 1),
+            //        Color.Empty, Color.Empty, angle, false))
+            //    {
+            //        switch (this.BarStyle)
+            //        {
+            //            case ColorBarStyle.TwoColor:
+            //                blend.Colors = new[]
+            //                {
+            //                    this.Color1,
+            //                    this.Color2
+            //                };
+            //                blend.Positions = new[]
+            //                {
+            //                    0F,
+            //                    1F
+            //                };
+            //                break;
+            //            case ColorBarStyle.ThreeColor:
+            //                blend.Colors = new[]
+            //                {
+            //                    this.Color1,
+            //                    this.Color2,
+            //                    this.Color3
+            //                };
+            //                blend.Positions = new[]
+            //                {
+            //                    0,
+            //                    0.5F,
+            //                    1
+            //                };
+            //                break;
+            //    }
+
+            //        brush.InterpolationColors = blend;
+            //        e.Graphics.FillRectangle(brush, this.BarBounds);
+            //    }
+            //}
         }
 
         #endregion
