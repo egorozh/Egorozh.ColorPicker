78e6fe3c8ed63786de6ce2949929bc8b73b7857b
Egor Zheludkov <zheludkov.egor@mail.ru>
2020-12-15 23:59:16 +0300

    add load and save palette buton icons

--- src/AvaloniaUI/Egorozh.ColorPicker.Avalonia/Controls/Palette/ColorPalette.cs
+++ src/AvaloniaUI/Egorozh.ColorPicker.Avalonia/Controls/Palette/ColorPalette.cs
@@ -1,10 +1,13 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml.Templates;
 using Avalonia.Media;
 using Avalonia.Styling;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Color = System.Drawing.Color;
 
 namespace Egorozh.ColorPicker
@@ -15,8 +18,11 @@ namespace Egorozh.ColorPicker
 
         private IColorManager? _colorManager;
 
-        private readonly MenuItem _addContextMenuItem;
-        private readonly MenuItem _removeItem;
+        private MenuItem _addContextMenuItem;
+        private MenuItem _removeItem;
+
+        private MenuItem _loadPaletteContextMenuItem;
+        private MenuItem _savePaletteContextMenuItem;
 
         private readonly ListBoxItem _addItem;
 
@@ -30,6 +36,18 @@ namespace Egorozh.ColorPicker
 
         #region Dependency Properties
 
+        public static readonly StyledProperty<IEnumerable<Avalonia.Media.Color>> ColorsProperty =
+            AvaloniaProperty.Register<ColorPreview, IEnumerable<Avalonia.Media.Color>>(
+                nameof(RemoveColorContextMenuText),
+                ColorPalettes.PaintPalette.Select(c => c.ToColor()),
+                notifying: ColorsPropertyChanged);
+
+        private static void ColorsPropertyChanged(IAvaloniaObject arg1, bool arg2)
+        {
+            if (arg1 is ColorPalette colorPalette)
+                colorPalette.ColorsPropertyChanged();
+        }
+
         public static readonly StyledProperty<string> RemoveColorContextMenuTextProperty =
             AvaloniaProperty.Register<ColorPreview, string>(nameof(RemoveColorContextMenuText), "Remove color");
 
@@ -39,9 +57,27 @@ namespace Egorozh.ColorPicker
         public static readonly StyledProperty<string> AddColorContextMenuTextProperty =
             AvaloniaProperty.Register<ColorPreview, string>(nameof(AddColorContextMenuText), "Add color");
 
+        public static readonly StyledProperty<DataTemplate> LoadPaletteIconTemplateProperty =
+            AvaloniaProperty.Register<ColorPreview, DataTemplate>(nameof(LoadPaletteIconTemplate));
+
+        public static readonly StyledProperty<DataTemplate> SavePaletteIconTemplateProperty =
+            AvaloniaProperty.Register<ColorPreview, DataTemplate>(nameof(SavePaletteIconTemplate));
+
+        public static readonly StyledProperty<string> LoadPaletteContextMenuTextProperty =
+            AvaloniaProperty.Register<ColorPreview, string>(nameof(LoadPaletteContextMenuText), "Load Palette");
+
+        public static readonly StyledProperty<string> SavePaletteContextMenuTextProperty =
+            AvaloniaProperty.Register<ColorPreview, string>(nameof(SavePaletteContextMenuText), "Save Palette");
+
         #endregion
 
         #region Public Properties
+
+        public IEnumerable<Avalonia.Media.Color> Colors
+        {
+            get => GetValue(ColorsProperty);
+            private set => SetValue(ColorsProperty, value);
+        }
 
         public string RemoveColorContextMenuText
         {
@@ -61,6 +97,30 @@ namespace Egorozh.ColorPicker
             private set => SetValue(AddColorContextMenuTextProperty, value);
         }
 
+        public DataTemplate LoadPaletteIconTemplate
+        {
+            get => GetValue(LoadPaletteIconTemplateProperty);
+            private set => SetValue(LoadPaletteIconTemplateProperty, value);
+        }
+
+        public DataTemplate SavePaletteIconTemplate
+        {
+            get => GetValue(SavePaletteIconTemplateProperty);
+            private set => SetValue(SavePaletteIconTemplateProperty, value);
+        }
+
+        public string LoadPaletteContextMenuText
+        {
+            get => GetValue(LoadPaletteContextMenuTextProperty);
+            private set => SetValue(LoadPaletteContextMenuTextProperty, value);
+        }
+
+        public string SavePaletteContextMenuText
+        {
+            get => GetValue(SavePaletteContextMenuTextProperty);
+            private set => SetValue(SavePaletteContextMenuTextProperty, value);
+        }
+
         #endregion
 
         #region Constructor
@@ -69,35 +129,10 @@ namespace Egorozh.ColorPicker
         {
             SelectionMode = SelectionMode.Multiple;
 
-            _removeItem = new()
-            {
-                Header = RemoveColorContextMenuText
-            };
-
-            _addContextMenuItem = new()
-            {
-                Header = AddColorContextMenuText
-            };
-
-            _removeItem.Click += RemoveItemOnClick;
-            _addContextMenuItem.Click += AddItemOnClick;
-
             _addItem = new ListBoxItem()
             {
                 Classes = new Classes("Add")
             };
-
-
-            ContextMenu = new ContextMenu()
-            {
-                Items = new List<MenuItem>
-                {
-                    _addContextMenuItem,
-                    _removeItem,
-                }
-            };
-
-            InitPalette(ColorPalettes.PaintPalette);
 
             _addItem.Tapped += AddItemOnTapped;
 
@@ -115,6 +150,28 @@ namespace Egorozh.ColorPicker
         public void Init(IColorManager colorManager)
         {
             _colorManager = colorManager;
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            base.OnApplyTemplate(e);
+
+            _addContextMenuItem = e.NameScope.Find<MenuItem>("PART_AddMenuItem");
+            _removeItem = e.NameScope.Find<MenuItem>("PART_RemoveMenuItem");
+
+            _loadPaletteContextMenuItem = e.NameScope.Find<MenuItem>("PART_LoadPaletteMenuItem");
+            _savePaletteContextMenuItem = e.NameScope.Find<MenuItem>("PART_SavePaletteMenuItem");
+
+            _removeItem.Click += RemoveItemOnClick;
+            _addContextMenuItem.Click += AddItemOnClick;
+            _loadPaletteContextMenuItem.Click += LoadPaletteContextMenuItemOnClick;
+            _savePaletteContextMenuItem.Click += SavePaletteContextMenuItemOnClick;
+
+            ColorsPropertyChanged();
         }
 
         #endregion
@@ -140,18 +197,18 @@ namespace Egorozh.ColorPicker
             if (Items is not IList<object> items)
                 return;
 
-            items.Insert(items.Count - 1, CreateColorItem(Colors.Transparent));
+            items.Insert(items.Count - 1, CreateColorItem(Avalonia.Media.Colors.Transparent));
         }
 
-        private void InitPalette(IEnumerable<Color> colors)
+        private void ColorsPropertyChanged()
         {
             if (Items is not IList<object> items)
                 return;
 
             items.Clear();
 
-            foreach (var color in colors)
-                items.Add(CreateColorItem(color.ToColor()));
+            foreach (var color in Colors)
+                items.Add(CreateColorItem(color));
 
             items.Add(_addItem);
         }
@@ -161,6 +218,7 @@ namespace Egorozh.ColorPicker
             if (Items is not IList<object> items)
                 return;
 
+            var newColorItem = CreateColorItem(Avalonia.Media.Colors.Transparent);
 
             if (SelectedItems.Count > 0)
             {
@@ -170,16 +228,16 @@ namespace Egorozh.ColorPicker
                 {
                     var index = items.IndexOf(selectedItem);
 
-                    items.Insert(index, CreateColorItem(Colors.Transparent));
+                    items.Insert(index, newColorItem);
                 }
                 else
                 {
-                    items.Insert(items.Count - 1, CreateColorItem(Colors.Transparent));
+                    items.Insert(items.Count - 1, newColorItem);
                 }
             }
             else
             {
-                items.Insert(items.Count - 1, CreateColorItem(Colors.Transparent));
+                items.Insert(items.Count - 1, newColorItem);
             }
         }
 
@@ -228,6 +286,14 @@ namespace Egorozh.ColorPicker
             removedItem.DoubleTapped -= ItemOnDoubleTapped;
 
             items.Remove(removedItem);
+        }
+
+        private void SavePaletteContextMenuItemOnClick(object? sender, RoutedEventArgs e)
+        {
+        }
+
+        private void LoadPaletteContextMenuItemOnClick(object? sender, RoutedEventArgs e)
+        {
         }
 
         #endregion
