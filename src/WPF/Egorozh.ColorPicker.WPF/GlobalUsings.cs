9dfe12769c22d24cdc0274c43346751169a5fb47
zheludkov <zheludkov@ddplanet.ru>
2023-08-19 23:25:14 +0300

    fix: fixed transparent brush shared

---
+++ src/WPF/Egorozh.ColorPicker.WPF/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Globalization;
+global using System.Linq;
+global using System.Runtime.InteropServices;
+global using System.Threading.Tasks;
